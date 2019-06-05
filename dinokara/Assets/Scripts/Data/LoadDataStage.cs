226b283f5721934cfb2c59c511719d7c8f65b759
admin <>
2019-06-06 01:57:04 +0900

    json関連追加

---
+++ dinokara/Assets/Scripts/Data/LoadDataStage.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class JsonHelper
+{
+    public static List<T> ListFromJson<T>(string json)
+    {
+        var newJson = "{ \"list\": " + json + "}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.list;
+    }
+
+    [Serializable]
+    class Wrapper<T>
+    {
+        public List<T> list;
+    }
+}
+
+[Serializable]
+public class Stage
+{
+    public int id;
+    public string stage_name;
+    public int time;
+    public string description;
+    public List<Block> block;
+}
+
+public class Block
+{
+    public int normal;
+    public int hard;
+}
+
+
+public class LoadDataStage : MonoBehaviour
+{
+    void Start()
+    {
+        string json = Resources.Load<TextAsset>("Data/stage").ToString();
+        var _stage = JsonHelper.ListFromJson<Stage>(json);
+        ;
+        Debug.Log(_stage[0].id);
+        Debug.Log(_stage[0].stage_name);
+
+    }
+}
