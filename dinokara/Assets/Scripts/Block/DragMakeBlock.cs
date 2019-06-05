214994ba2420065e866ca861bac6d40e9ec8b1a2
admin <>
2019-06-05 10:12:28 +0900

    ブロック移動処理、その他構造など修正

---
+++ dinokara/Assets/Scripts/Block/DragMakeBlock.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DinokarA.Assets.Scripts.Block
+{
+
+    public class DragMakeBlock : MonoBehaviour
+    {
+        [SerializeField] private GameObject _block;
+
+        [SerializeField] private bool makeFlag = false;
+
+        private void OnMouseDown()
+        {
+            if (!makeFlag)
+            {
+                var _position = gameObject.transform.position;
+                Instantiate(_block, _position, Quaternion.identity);
+                makeFlag = true;
+            }
+        }
+
+        private void OnMouseDrag()
+        {
+            Vector3 objectPointInScreen
+                = Camera.main.WorldToScreenPoint(this.transform.position);
+
+            Vector3 mousePointInScreen
+                = new Vector3(Input.mousePosition.x,
+                    Input.mousePosition.y,
+                    objectPointInScreen.z);
+
+            Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
+            mousePointInWorld.z = this.transform.position.z;
+            this.transform.position = mousePointInWorld;
+        }
+
+    }
+
+}
---
+++ dinokara/Assets/Scripts/UI/ButtonSceneMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace DinokarA.Assets.Scripts.UI
+{
+    public class ButtonSceneMove : MonoBehaviour
+    {
+        [SerializeField] private string loadScene;
+
+        public void PushButton()
+        {
+            SceneManager.LoadScene(loadScene);
+        }
+    }
+}
