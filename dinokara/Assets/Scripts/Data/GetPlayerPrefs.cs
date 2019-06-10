8c938e683d49ad431a0ce41d91fa2dca09528fe7
admin <godhandevilleg+github@gmail.com>
2019-06-10 14:38:58 +0900

    軽微な修正

--- dinokara/Assets/Scripts/Data/GetPlayerPrefs.cs
+++ dinokara/Assets/Scripts/Data/GetPlayerPrefs.cs
@@ -5,12 +5,11 @@ namespace DinokarA.Assets.Scripts.Data
     public class GetPlayerPrefs : MonoBehaviour
     {
         // 現在のステージ名を取得
-        static public string GetCurrentStage()
+        public static string GetCurrentStage()
         {
             var _stage_name = PlayerPrefs.GetString("CurrentStage", "Stage1");
 
             return _stage_name;
         }
-        
     }
 }
